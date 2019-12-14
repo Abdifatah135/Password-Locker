adb5a08db910b8fb6550f0af2bade5a659cff9c9
Abdi <oxabdi75@gmail.com>
2019-12-15 02:24:26 +0300

    added test instance asnd testcase

--- user_test.py
+++ user_test.py
@@ -14,3 +14,15 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("James","Muriuki") # create user object
+        
+        
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name,"James")
+        self.assertEqual(self.new_user.password,"Muriuki")
+        
+if __name__ == '__main__':
+    unittest.main()
